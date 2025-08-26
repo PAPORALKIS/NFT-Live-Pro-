e29d9292eed4c8476cbccb9c09e66fbce6f9a480
PAPORALKIS <k.paporalkis@gmail.com>
2025-08-26 23:25:31 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -30,4 +30,3 @@ function generatePDF(){
   const opt={margin:0.5,filename:'Rapport_NFT_Ultime_Pro.pdf',image:{type:'jpeg',quality:0.98},html2canvas:{scale:2,useCORS:true},jsPDF:{unit:'in',format:'a4',orientation:'portrait'}};
   html2pdf().set(opt).from(document.body).save();
 }
-
